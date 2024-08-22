a10fb25dde3778b25ee102b7b3786eacda75d227
Mohammed-Jomaa <147437360+Mohammed-Jomaa@users.noreply.github.com>
2024-08-22 23:31:04 +0300

    first

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult getAll()
+        {
+            return View();
+        }
+        public IActionResult create()
+        {
+            return View();
+        }
+        public IActionResult update()
+        {
+            return View();
+        }
+        public IActionResult delete()
+        {
+            return View();
+        }
+    }
+}
